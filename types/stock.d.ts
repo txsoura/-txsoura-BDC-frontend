d02921e45abe86fc16dc36e236e5046a5de09b4f
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-11-16 19:47:59 -0300

    feat(types): create stock

---
+++ types/stock.d.ts
@@ -0,0 +1,25 @@
+import {ConstructionProps} from "./construction";
+import {ProductProps} from "./product";
+import {ProviderProps} from "./provider";
+
+export interface StockProps {
+    id: number;
+    quantity: number;
+    price: number;
+    construction_id: number;
+    construction: ConstructionProps;
+    provider_id: number;
+    provider: ProviderProps;
+    product_id: number;
+    product: ProductProps;
+    flow: string;
+    status: string;
+    outgoing_receiver?: string;
+    receipt?: string;
+    canceled_at?: Date;
+    received_at?: Date;
+    withdrawn_at?: Date;
+    created_at: Date;
+    updated_at: Date;
+    deleted_at?: Date;
+}
