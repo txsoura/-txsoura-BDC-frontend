1d85cc512c983f688c223df0918f18a32a85faf4
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-11-16 19:47:00 -0300

    feat(types): create pagination

---
+++ types/pagination.d.ts
@@ -0,0 +1,10 @@
+export interface MetaProps {
+    current_page: number;
+    from: number;
+    last_page: number;
+    links: [];
+    path: string;
+    per_page: number;
+    to: number;
+    total: number;
+}
