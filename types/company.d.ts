43e9b78097b17fe41645b7f92252bc0bcaeb5874
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-11-16 19:44:40 -0300

    feat(types): create company

---
+++ types/company.d.ts
@@ -0,0 +1,21 @@
+export interface CompanyProps {
+    id: number;
+    name: string;
+    tax: string;
+    type: string;
+    workspace: string;
+    cellphone?: number;
+    email: string;
+    street?: string;
+    postcode?: string;
+    number?: string;
+    complement?: string;
+    city?: string;
+    state?: string;
+    country?: string;
+    district?: string;
+    status: string;
+    created_at: Date;
+    updated_at: Date;
+    deleted_at?: Date;
+}
