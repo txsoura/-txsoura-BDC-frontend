b06581ef50856350eea0830f060b25a801d64d89
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-11-20 18:39:22 -0300

    feat(hooks): create loading

---
+++ hooks/loading.tsx
@@ -0,0 +1,31 @@
+import React, {createContext, useContext, useState} from 'react';
+
+interface ContextProps {
+    loading: boolean;
+
+    setLoading(_value: boolean): void;
+}
+
+const Loading = createContext<ContextProps>({} as ContextProps);
+
+const LoadingProvider = ({children}) => {
+    const [loading, setLoading] = useState<boolean>(true);
+
+    return (
+        <Loading.Provider value={{loading, setLoading}}>
+            {children}
+        </Loading.Provider>
+    );
+};
+
+function useLoading() {
+    const context = useContext(Loading);
+
+    if (!context) {
+        throw new Error('useLoading must be used within a LoadingProvider');
+    }
+
+    return context;
+}
+
+export {LoadingProvider, useLoading};
