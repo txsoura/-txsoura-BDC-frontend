25f455082326101d6fa9b04b98868002e3eb5daa
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-11-20 18:40:41 -0300

    feat(service): create api instance

---
+++ services/api.ts
@@ -0,0 +1,22 @@
+import {createInstance, createRefreshInterceptor} from '/utils/apiConfig';
+
+interface apiURL {
+    development: string;
+    production: string;
+}
+
+const authUrl: apiURL = {
+    development: process.env.NEXT_PUBLIC_SERVICE_URL,
+    production: process.env.NEXT_PUBLIC_SERVICE_URL,
+};
+
+const serviceURL: apiURL = {
+    development: process.env.NEXT_PUBLIC_SERVICE_URL + '/api/v1',
+    production: process.env.NEXT_PUBLIC_SERVICE_URL + '/api/v1',
+};
+
+export const authAPI = createInstance(authUrl);
+export const serviceAPI = createInstance(serviceURL);
+
+createRefreshInterceptor(authAPI);
+createRefreshInterceptor(serviceAPI);
