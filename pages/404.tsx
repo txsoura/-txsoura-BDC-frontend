295c2bf233b6a074af60af89f6ede4cd9f52116b
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-11-22 18:00:08 -0300

    feat(pages): create 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,40 +1,43 @@
-import {useAuth} from "/hooks/auth";
+import { useAuth } from 'hooks/auth';
+import { useRouter } from 'next/router';
 
 const Index = () => {
-    const {user} = useAuth();
+  const { user } = useAuth();
+  const router = useRouter();
 
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-            <div className="max-w-md w-full space-y-8">
-                <div>
-                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Oops, página não
-                        encontrada!!!</h2>
-                </div>
-
-                <div>
-                    {(user && user.role === "user") && (
-                        <a href='/dashboard'>
-                            <button
-                                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                            >
-                                Dashboard
-                            </button>
-                        </a>
-                    )}
-
-                    {(user && user.role === "admin") && (
-                        <a href='/admin'>
-                            <button
-                                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                            >
-                                Dashboard
-                            </button>
-                        </a>
-                    )}
-                </div>
-            </div>
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Oops, página não encontrada!!!
+          </h2>
         </div>
-    )
-}
+
+        <div>
+          {user && user.role === 'user' && (
+            <button
+              type="button"
+              onClick={() => router.push('/dashboard')}
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Dashboard
+            </button>
+          )}
+
+          {user && user.role === 'admin' && (
+            <button
+              type="button"
+              onClick={() => router.push('/admin')}
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Dashboard
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Index;
