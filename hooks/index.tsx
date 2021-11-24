de01cbdefbb68c69a2b011b3b7cae9a3e4e52071
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-11-24 00:46:35 -0300

    feat(hooks): index

--- hooks/index.tsx
+++ hooks/index.tsx
@@ -1,14 +1,12 @@
-import {AuthProvider} from './auth';
-import {LoadingProvider} from "./loading";
+import { AuthProvider } from './auth';
+import { LoadingProvider } from './loading';
 
-const HooksProvider = ({children}) => {
-    return (
-        <LoadingProvider>
-            <AuthProvider>
-                {children}
-            </AuthProvider>
-        </LoadingProvider>
-    )
-}
+const HooksProvider = ({ children }) => {
+  return (
+    <LoadingProvider>
+      <AuthProvider>{children}</AuthProvider>
+    </LoadingProvider>
+  );
+};
 
 export default HooksProvider;
