f2c6e37cd11747a128d35d1cc8771907be2a6e7d
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-11-24 00:44:44 -0300

    feat(component): header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,31 +1,36 @@
-import Head from "next/head";
+import Head from 'next/head';
 
-const Index = ({title, button = false, buttonText = 'Button', buttonHref = '#'}) => {
-    return (
-        <>
-            <Head>
-                <title>{title}</title>
-            </Head>
+const Index = ({
+  title,
+  button = false,
+  buttonText = 'Button',
+  buttonHref = '#',
+}) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
 
-            <header className="bg-white shadow">
-                <div className="sm:flex sm:justify-between">
-                    <div className="max-w-7xl mx-auto py-6 px- sm:px-6 lg:px-8 ml-0">
-                        <h1 className="text-3xl font-bold text-gray-900">{title}</h1>
-                    </div>
-                    {(button && (
-                        <div className="inline-flex rounded-md">
-                            <a
-                                href={buttonHref}
-                                className="inline-flex items-center justify-center mr-5 mt-5 px-5 h-10 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
-                            >
-                                {buttonText}
-                            </a>
-                        </div>
-                    ))}
-                </div>
-            </header>
-        </>
-    )
-}
+      <header className="bg-white shadow">
+        <div className="sm:flex sm:justify-between">
+          <div className="max-w-7xl mx-auto py-6 px- sm:px-6 lg:px-8 ml-0">
+            <h1 className="text-3xl font-bold text-gray-900">{title}</h1>
+          </div>
+          {button && (
+            <div className="inline-flex rounded-md">
+              <a
+                href={buttonHref}
+                className="inline-flex items-center justify-center mr-5 mt-5 px-5 h-10 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+              >
+                {buttonText}
+              </a>
+            </div>
+          )}
+        </div>
+      </header>
+    </>
+  );
+};
 
 export default Index;
