d4422cb64b414cbc920731b53c809614d6605311
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-11-24 00:50:19 -0300

    feat(types): user

--- types/user.d.ts
+++ types/user.d.ts
@@ -1,13 +1,13 @@
 export interface UserProps {
-    id: number;
-    name: string;
-    email: string;
-    status: string;
-    fcm_token?: string;
-    role: string;
-    lang: string;
-    email_verified_at?: Date;
-    created_at: Date;
-    updated_at: Date;
-    deleted_at?: Date;
+  id: number;
+  name: string;
+  email: string;
+  status: string;
+  fcm_token?: string;
+  role: string;
+  lang: string;
+  email_verified_at?: Date;
+  created_at: Date;
+  updated_at: Date;
+  deleted_at?: Date;
 }
