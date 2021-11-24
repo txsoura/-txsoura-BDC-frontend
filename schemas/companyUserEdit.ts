23e786a4666a286fdf5f2d71380e58e36a4f52be
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-11-24 00:30:08 -0300

    feat(schemas): company user edit

--- schemas/companyUserEdit.ts
+++ schemas/companyUserEdit.ts
@@ -1,7 +1,7 @@
 import * as Yup from 'yup';
 
 const companyUserEditSchema = Yup.object().shape({
-    role: Yup.string().ensure().required('Este campo, é obrigatório'),
+  role: Yup.string().ensure().required('Este campo, é obrigatório'),
 });
 
 export default companyUserEditSchema;
