c303450f3c128c18e9bbe7ca55e3f93853d7af2e
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-11-24 00:39:26 -0300

    feat(types): construction

--- types/construction.d.ts
+++ types/construction.d.ts
@@ -1,20 +1,20 @@
-import {CompanyProps} from "./company";
+import { CompanyProps } from './company';
 
 export interface ConstructionProps {
-    id: number;
-    name: string;
-    start_date: Date;
-    end_date: Date;
-    status: string;
-    budget: number;
-    project?: string;
-    canceled_at?: Date;
-    started_at?: Date;
-    finalized_at?: Date;
-    abandoned_at?: Date;
-    company_id: number;
-    company: CompanyProps;
-    created_at: Date;
-    updated_at: Date;
-    deleted_at?: Date;
+  id: number;
+  name: string;
+  start_date: Date;
+  end_date: Date;
+  status: string;
+  budget: number;
+  project?: string;
+  canceled_at?: Date;
+  started_at?: Date;
+  finalized_at?: Date;
+  abandoned_at?: Date;
+  company_id: number;
+  company: CompanyProps;
+  created_at: Date;
+  updated_at: Date;
+  deleted_at?: Date;
 }
