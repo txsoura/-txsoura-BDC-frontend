bd8733a4a4d54d8cde44f7eaa704b5488a32c3b3
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-11-24 00:28:10 -0300

    feat(component): dashboard bar chart

--- components/DashboardCard/barChart.tsx
+++ components/DashboardCard/barChart.tsx
@@ -1,49 +1,49 @@
 import React from 'react';
 
-import BarChart from '/components/Charts/bar';
+import BarChart from 'components/Charts/bar.js';
 
 function Index() {
-    const chartData = {
-        labels: [
-            '12-01-2020', '01-01-2021', '02-01-2021',
-            '03-01-2021', '04-01-2021', '05-01-2021',
-        ],
-        datasets: [
-            // Light blue bars
-            {
-                label: 'Direct',
-                data: [
-                    800, 1600, 900, 1300, 1950, 1700,
-                ],
-                backgroundColor: '#60A5FA',
-                hoverBackgroundColor: '#3B82F6',
-                barPercentage: 0.66,
-                categoryPercentage: 0.66,
-            },
-            // Blue bars
-            {
-                label: 'Indirect',
-                data: [
-                    4900, 2600, 5350, 4800, 5200, 4800,
-                ],
-                backgroundColor: '#6366F1',
-                hoverBackgroundColor: '#4F46E5',
-                barPercentage: 0.66,
-                categoryPercentage: 0.66,
-            },
-        ],
-    };
+  const chartData = {
+    labels: [
+      '12-01-2020',
+      '01-01-2021',
+      '02-01-2021',
+      '03-01-2021',
+      '04-01-2021',
+      '05-01-2021',
+    ],
+    datasets: [
+      // Light blue bars
+      {
+        label: 'Direct',
+        data: [800, 1600, 900, 1300, 1950, 1700],
+        backgroundColor: '#60A5FA',
+        hoverBackgroundColor: '#3B82F6',
+        barPercentage: 0.66,
+        categoryPercentage: 0.66,
+      },
+      // Blue bars
+      {
+        label: 'Indirect',
+        data: [4900, 2600, 5350, 4800, 5200, 4800],
+        backgroundColor: '#6366F1',
+        hoverBackgroundColor: '#4F46E5',
+        barPercentage: 0.66,
+        categoryPercentage: 0.66,
+      },
+    ],
+  };
 
-    return (
-        <div className="flex flex-col col-span-full sm:col-span-6 bg-white shadow-lg rounded-sm border border-gray-200">
-            <header className="px-5 py-4 border-b border-gray-100">
-                <h2 className="font-semibold text-gray-800">Direct VS Indirect</h2>
-            </header>
-            {/* Chart built with Chart.js 3 */}
-            {/* Change the height attribute to adjust the chart height */}
-            <BarChart data={chartData} width={595} height={248}/>
-        </div>
-    );
+  return (
+    <div className="flex flex-col col-span-full sm:col-span-6 bg-white shadow-lg rounded-sm border border-gray-200">
+      <header className="px-5 py-4 border-b border-gray-100">
+        <h2 className="font-semibold text-gray-800">Direct VS Indirect</h2>
+      </header>
+      {/* Chart built with Chart.js 3 */}
+      {/* Change the height attribute to adjust the chart height */}
+      <BarChart data={chartData} width={595} height={248} />
+    </div>
+  );
 }
 
 export default Index;
