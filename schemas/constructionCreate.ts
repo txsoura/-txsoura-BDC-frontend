be2fd9c6c711308160ad5364ac9f07a57b6a03b2
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-11-24 00:30:28 -0300

    feat(schemas): construction create

--- schemas/constructionCreate.ts
+++ schemas/constructionCreate.ts
@@ -1,10 +1,14 @@
 import * as Yup from 'yup';
 
 const constructionCreateSchema = Yup.object().shape({
-    name: Yup.string().required('Este campo, é obrigatório').max(255, 'Este campo deve ter no máximo 255 caracteres'),
-    date_start: Yup.date().required('Este campo, é obrigatório'),
-    date_end: Yup.date().required('Este campo, é obrigatório'),
-    budget: Yup.number().required('Este campo, é obrigatório').positive('Este campo, não pode receber valores abaixo de 0'),
+  name: Yup.string()
+    .required('Este campo, é obrigatório')
+    .max(255, 'Este campo deve ter no máximo 255 caracteres'),
+  date_start: Yup.date().required('Este campo, é obrigatório'),
+  date_end: Yup.date().required('Este campo, é obrigatório'),
+  budget: Yup.number()
+    .required('Este campo, é obrigatório')
+    .positive('Este campo, não pode receber valores abaixo de 0'),
 });
 
 export default constructionCreateSchema;
