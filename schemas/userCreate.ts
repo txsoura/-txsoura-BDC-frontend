1bac435e3dde0b8d30048d79013152245d55c4cc
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-11-24 00:32:07 -0300

    feat(schemas): user create

--- schemas/userCreate.ts
+++ schemas/userCreate.ts
@@ -1,9 +1,14 @@
 import * as Yup from 'yup';
 
 const userCreateSchema = Yup.object().shape({
-    name: Yup.string().required('Este campo, é obrigatório').max(255, 'Este campo deve ter no máximo 255 caracteres'),
-    email: Yup.string().email('Este campo, deve ser um email válido').required('Este campo, é obrigatório').max(255, 'Este campo, deve ter no máximo 255 caracteres'),
-    role: Yup.string().ensure().required('Este campo, é obrigatório'),
+  name: Yup.string()
+    .required('Este campo, é obrigatório')
+    .max(255, 'Este campo deve ter no máximo 255 caracteres'),
+  email: Yup.string()
+    .email('Este campo, deve ser um email válido')
+    .required('Este campo, é obrigatório')
+    .max(255, 'Este campo, deve ter no máximo 255 caracteres'),
+  role: Yup.string().ensure().required('Este campo, é obrigatório'),
 });
 
 export default userCreateSchema;
