17237551e935355bbcb722b5d5ee3c6558dccc4b
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-11-24 00:35:22 -0300

    feat(component): with nav bar layout

--- components/withNavBarLayout.tsx
+++ components/withNavBarLayout.tsx
@@ -1,12 +1,12 @@
 import NavBar from './NavBar';
 
 export const withNavBarLayout = (WrappedComponent, navbar = true) => {
-    return props => {
-        return (
-            <>
-                {navbar && <NavBar/>}
-                <WrappedComponent {...props} />
-            </>
-        );
-    };
-}
+  return props => {
+    return (
+      <>
+        {navbar && <NavBar />}
+        <WrappedComponent {...props} />
+      </>
+    );
+  };
+};
