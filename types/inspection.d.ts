515892bae9fba1764913499339330cf37cd279e1
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-11-24 00:46:51 -0300

    feat(types): inspection

--- types/inspection.d.ts
+++ types/inspection.d.ts
@@ -1,12 +1,12 @@
-import {ConstructionProps} from "./construction";
+import { ConstructionProps } from './construction';
 
 export interface InspectionProps {
-    id: number;
-    construction_id: number;
-    construction: ConstructionProps;
-    seem: string;
-    report?: string;
-    created_at: Date;
-    updated_at: Date;
-    deleted_at?: Date;
+  id: number;
+  construction_id: number;
+  construction: ConstructionProps;
+  seem: string;
+  report?: string;
+  created_at: Date;
+  updated_at: Date;
+  deleted_at?: Date;
 }
