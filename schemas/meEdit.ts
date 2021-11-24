aa810e8eb5d551e064186cbd860c57a7848e6c4e
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-11-24 00:31:14 -0300

    feat(schemas): me edit

--- schemas/meEdit.ts
+++ schemas/meEdit.ts
@@ -1,8 +1,13 @@
 import * as Yup from 'yup';
 
 const meEditSchema = Yup.object().shape({
-    name: Yup.string().required('Este campo, é obrigatório').max(255, 'Este campo deve ter no máximo 255 caracteres'),
-    email: Yup.string().email('Este campo, deve ser um email válido').required('Este campo, é obrigatório').max(255, 'Este campo, deve ter no máximo 255 caracteres'),
+  name: Yup.string()
+    .required('Este campo, é obrigatório')
+    .max(255, 'Este campo deve ter no máximo 255 caracteres'),
+  email: Yup.string()
+    .email('Este campo, deve ser um email válido')
+    .required('Este campo, é obrigatório')
+    .max(255, 'Este campo, deve ter no máximo 255 caracteres'),
 });
 
 export default meEditSchema;
