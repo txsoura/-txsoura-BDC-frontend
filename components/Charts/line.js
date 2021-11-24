ad0c2c7544a12fd1a0bac77b771c1488da5b4ba3
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-11-24 00:27:40 -0300

    feat(components): charts

--- components/Charts/line.js
+++ components/Charts/line.js
@@ -1,71 +1,81 @@
-import React, {useEffect, useRef} from 'react';
-import {Chart, Filler, LinearScale, LineController, LineElement, PointElement, TimeScale, Tooltip,} from 'chart.js';
+import React, { useEffect, useRef } from 'react';
+import {
+  Chart,
+  Filler,
+  LinearScale,
+  LineController,
+  LineElement,
+  PointElement,
+  TimeScale,
+  Tooltip,
+} from 'chart.js';
 import 'chartjs-adapter-moment';
 
-import {currencyFormatter} from "/utils/currencyFormatter";
+import { currencyFormatter } from 'utils/currencyFormatter';
 
-Chart.register(LineController, LineElement, Filler, PointElement, LinearScale, TimeScale, Tooltip);
+Chart.register(
+  LineController,
+  LineElement,
+  Filler,
+  PointElement,
+  LinearScale,
+  TimeScale,
+  Tooltip,
+);
 
-function Index({
-                   data,
-                   width,
-                   height
-               }) {
+function Index({ data, width, height }) {
+  const canvas = useRef(null);
 
-    const canvas = useRef(null);
-
-    useEffect(() => {
-        const ctx = canvas.current;
-        // eslint-disable-next-line no-unused-vars
-        const chart = new Chart(ctx, {
-            type: 'line',
-            data: data,
-            options: {
-                chartArea: {
-                    backgroundColor: '#F9FAFB',
-                },
-                layout: {
-                    padding: 20,
-                },
-                scales: {
-                    y: {
-                        display: false,
-                        beginAtZero: true,
-                    },
-                    x: {
-                        type: 'time',
-                        time: {
-                            parser: 'MM-DD-YYYY',
-                            unit: 'month',
-                        },
-                        display: false,
-                    },
-                },
-                plugins: {
-                    tooltip: {
-                        callbacks: {
-                            title: () => false, // Disable tooltip title
-                            label: (context) => currencyFormatter(context.parsed.y),
-                        },
-                    },
-                    legend: {
-                        display: false,
-                    },
-                },
-                interaction: {
-                    intersect: false,
-                    mode: 'nearest',
-                },
-                maintainAspectRatio: false,
+  useEffect(() => {
+    const ctx = canvas.current;
+    // eslint-disable-next-line no-unused-vars
+    const chart = new Chart(ctx, {
+      type: 'line',
+      data,
+      options: {
+        chartArea: {
+          backgroundColor: '#F9FAFB',
+        },
+        layout: {
+          padding: 20,
+        },
+        scales: {
+          y: {
+            display: false,
+            beginAtZero: true,
+          },
+          x: {
+            type: 'time',
+            time: {
+              parser: 'MM-DD-YYYY',
+              unit: 'month',
             },
-        });
-        return () => chart.destroy();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+            display: false,
+          },
+        },
+        plugins: {
+          tooltip: {
+            callbacks: {
+              title: () => false, // Disable tooltip title
+              label: context => currencyFormatter(context.parsed.y),
+            },
+          },
+          legend: {
+            display: false,
+          },
+        },
+        interaction: {
+          intersect: false,
+          mode: 'nearest',
+        },
+        maintainAspectRatio: false,
+      },
+    });
+    return () => chart.destroy();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    return (
-        <canvas ref={canvas} width={width} height={height}></canvas>
-    );
+  return <canvas ref={canvas} width={width} height={height} />;
 }
 
 export default Index;
