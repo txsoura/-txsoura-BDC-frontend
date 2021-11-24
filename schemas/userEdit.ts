4f5902c15511e09e99f255673c2a9be970d762d6
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-11-24 00:32:14 -0300

    feat(schemas): user edit

--- schemas/userEdit.ts
+++ schemas/userEdit.ts
@@ -1,8 +1,10 @@
 import * as Yup from 'yup';
 
 const userEditSchema = Yup.object().shape({
-    name: Yup.string().required('Este campo, é obrigatório').max(255, 'Este campo deve ter no máximo 255 caracteres'),
-    role: Yup.string().ensure().required('Este campo, é obrigatório'),
+  name: Yup.string()
+    .required('Este campo, é obrigatório')
+    .max(255, 'Este campo deve ter no máximo 255 caracteres'),
+  role: Yup.string().ensure().required('Este campo, é obrigatório'),
 });
 
 export default userEditSchema;
