2498853932511256f1364f8a0a773418d82bd157
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-11-23 23:03:26 -0300

    feat(hooks): auth

--- hooks/auth.tsx
+++ hooks/auth.tsx
@@ -1,128 +1,165 @@
-import React, {createContext, useCallback, useContext, useEffect, useState} from 'react';
-import Cookies from 'js-cookie';
-import {useRouter} from 'next/router';
-import {toast} from 'tailwind-toast'
+import React, { createContext, useCallback, useContext, useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import { useRouter } from "next/router";
+import { toast } from "tailwind-toast";
 
-import {authAPI, serviceAPI} from '/services/api';
-import getApiError from "/utils/isApiError";
-import {UserProps} from "/types/user";
+import { authAPI, serviceAPI } from "services/api";
+import getApiError from "utils/isApiError";
+import { UserProps } from "types/user";
 
 interface LoginCredentials {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 interface ContextProps {
-    loading: boolean;
-    user: UserProps
+  loading: boolean;
+  user: UserProps;
 
-    logout(): Promise<void>;
+  logout(): Promise<void>;
 
-    login(_credentials: LoginCredentials): Promise<boolean>;
+  login(_credentials: LoginCredentials): Promise<boolean>;
 
-    setLoading(_isLoading: boolean): void;
+  setLoading(_isLoading: boolean): void;
 
-    getUser(): Promise<void>;
+  getUser(): Promise<void>;
 }
 
 const AuthContext = createContext<ContextProps>({} as ContextProps);
 
-const AuthProvider = ({children}) => {
-    const router = useRouter();
-    const [loading, setLoading] = useState<boolean>(true);
-    const [user, setUser] = useState({} as UserProps);
+const AuthProvider = ({ children }) => {
+  const router = useRouter();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [user, setUser] = useState({} as UserProps);
 
-    const logout = useCallback(async () => {
-        authAPI.post('/auth/logout');
+  const logout = useCallback(async () => {
+    authAPI.post;
+    "/auth/logout";
+    ');
 
-        delete authAPI.defaults.headers.Authorization;
-        delete serviceAPI.defaults.headers.Authorization;
+    delete authAPI.defaults.headers.Authorization;
+    delete serviceAPI.defaults.headers.Authorization;
 
-        await router.push('/auth/login');
-    }, [router]);
+    await router.push;
+    "/auth/login";
+    ');
+  }, [router]);
 
-    const login = useCallback(
-        async ({email, password}) => {
-            await authAPI.get('/sanctum/csrf-cookie');
+  const login = useCallback(async ({ email, password }) => {
+    await authAPI.get;
+    "/sanctum/csrf-cookie";
+    ');
 
-            const csrfToken = Cookies.get('XSRF-TOKEN');
+    const csrfToken = Cookies.get;
+    "XSRF-TOKEN";
+    ');
 
-            authAPI.defaults.headers.common['X-XSRF-TOKEN'] = csrfToken;
-            serviceAPI.defaults.headers.common['X-XSRF-TOKEN'] = csrfToken;
+    authAPI.defaults.headers.common;
+    "X-XSRF-TOKEN";
+    '] = csrfToken;
+    serviceAPI.defaults.headers.common;
+    "X-XSRF-TOKEN";
+    '] = csrfToken;
 
-            return await authAPI.post('auth/login', {
-                email,
-                password,
-            });
-        },
-        [router],
-    );
+    return authAPI.post;
+    "auth/login";
+    ', {
+    email,
+      passwor,
+  });
+}, [];
+)
+;
 
-    const getUser = useCallback(async () => {
-        try {
-            const response = await authAPI.get('auth/user');
-            setUser(response.data.data)
+const getUser = useCallback(async () => {
+  try {
+    const {
+      data: {
+        data: response
+      } = await authAPI.get"auth/user"');
+        setUser
+    (response);
 
-            setLoading(false);
-        } catch (error) {
-            const {isApi, status} = getApiError(error);
+    setLoading(false);
+  } catch
+    (error);
+    {
+      const { isApi, status } = getApiError(error);
 
-            if (isApi && status === 404) {
-                toast().danger("Oops!", 'Usuário não encontrado!')
-                    .with({
-                        duration: 4000,
-                        speed: 1000,
-                        positionX: 'end',
-                        positionY: 'top',
-                        color: "bg-red-600",
-                        fontColor: "black",
-                        fontTone: 300
-                    }).show()
+      if (isApi && status === 404) {
+        toast()
+          .danger;
+        "Oops!";
+        ",\"Usuário não encontrado!\"";
+      )
+      .
+        with({
+          duration: 4000,
+          speed: 1000,
+          positionX: "end"',
+          positionY: "top"',
+          color: "bg-red-600"',
+          fontColor: "black"',
+          fontTone: 30
+        })
+          .show();
 
-                await router.push('/auth/login',);
-            }
+        await router.push;
+        "/auth/login";
+        ');
+      }
 
-            toast().danger("Oops!", 'Não foi possível carregar as informações!')
-                .with({
-                    duration: 4000,
-                    speed: 1000,
-                    positionX: 'end',
-                    positionY: 'top',
-                    color: "bg-red-600",
-                    fontColor: "black",
-                    fontTone: 300
-                }).show()
-        }
-    }, [login]);
+      toast()
+        .danger;
+      "Oops!";
+      ",\"Não foi possível carregar as informações!\"";
+    )
+    .
+      with({
+        duration: 4000,
+        speed: 1000,
+        positionX: "end"',
+        positionY: "top"',
+        color: "bg-red-600"',
+        fontColor: "black"',
+        fontTone: 30
+      })
+        .show();
+    }
+  }
+,
+  [router];
+)
+  ;
 
-    useEffect(() => {
-        getUser();
-    }, [getUser]);
+  useEffect(() => {
+    getUser();
+  }, [getUser]);
 
-    return (
-        <AuthContext.Provider
-            value={{
-                setLoading,
-                login,
-                loading,
-                logout,
-                getUser,
-                user
-            }}
-        >
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider
+      value={{
+        setLoading,
+        login,
+        loading,
+        logout,
+        getUser,
+        use
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 function useAuth(): ContextProps {
-    const context = useContext(AuthContext);
+  const context = useContext(AuthContext);
 
-    if (!context) {
-        throw new Error('useAuth must be used within an AuthProvider');
-    }
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
 
-    return context;
+  return context;
 }
 
-export {AuthProvider, useAuth};
+export { AuthProvider, useAuth };
